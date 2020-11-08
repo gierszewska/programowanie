46736513c94828f2c36a357a604e1b65fdeeed15
gierszewska <72295311+gierszewska@users.noreply.github.com>
2020-11-08 09:00:54 +0100

    2

--- Programowanie/PHP/Lekcja_3/ex_3/zmienne_globalne.php
+++ Programowanie/PHP/Lekcja_3/ex_3/zmienne_globalne.php
@@ -7,26 +7,34 @@ Wynik wyświetl z precyzją dwóch miejsc.
 Pole wynik jest nieaktywne, tylko wyświetla wynik.
 Wartość początkowa wyniku wynosi 0.
  -->
+<?php
+session_start();
+if (!isset($_SESSION['result'])) {
+$_SESSION['result'] = 0;
+}
+?>
 
- <!DOCTYPE html>
- <html lang="en">
- <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Dodawanie / odejmowanie</title>
- </head>
- <body>
-   <form name="form1" method="POST">
+<!DOCTYPE html>
+<html lang="en">
 
-   <label for="liczba">Liczba:</label>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dodawanie / odejmowanie</title>
+</head>
+
+<body>
+  <form name="form1" method="POST">
+
+    <label for="liczba">Liczba:</label>
     <input type="number" name="liczba" value="0"><br><br>
 
-   <label for="radio">Rodzaj działania:</label><br>
-   <label for="dodawanie">Dodawanie</label>
+    <label for="radio">Rodzaj działania:</label><br>
+    <label for="dodawanie">Dodawanie</label>
     <input type="radio" name="metoda" value="add" checked><br>
-   <label for="odejmowanie">Odejmowanie</label>
+    <label for="odejmowanie">Odejmowanie</label>
     <input type="radio" name="metoda" value="sub"><br><br>
-<?php
+    <?php
     function add(int $num1,int $num2)
     {
         $num1+=$num2;
@@ -38,10 +46,9 @@ Wartość początkowa wyniku wynosi 0.
         $num1-=$num2;
         return $num1;
     }
-
+    $ans = 0;
     if (isset($_POST['button'])) {
       $num = $_POST['liczba'];
-      $ans = 0;
 
       switch ($_POST['metoda']) {
         case 'add':
@@ -61,12 +68,13 @@ Wartość początkowa wyniku wynosi 0.
 
   ?>
 
-   <label for="wynik">Wynik:</label>
+    <label for="wynik">Wynik:</label>
     <!-- <input type="number" name="wynik" placeholder="0" id="wynik" value="0" ><br><br> -->
-    <input type="text" name="wynik" id="echo" value="<?php echo $ans;?>"readonly><br><br>
-   <input type="submit" name="button" value="Oblicz">
+    <input type="text" name="wynik" id="echo" value="<?php
+      echo $ans;?>" readonly><br><br>
+    <input type="submit" name="button" value="Oblicz">
 
-   </form>
+  </form>
+</body>
 
- </body>
- </html>
+</html>
